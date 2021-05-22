37045ee2bb72829c565ba44c8a811afddbf2e4e0
Ugurcan Turk <ugurcan.uut82@gmail.com>
2021-05-22 14:40:12 +0300

    header component

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCM43bvTTPw234Wht-AEdg6IgtMhR-5JGI",
+    authDomain: "fir-6f18d.firebaseapp.com",
+    projectId: "fir-6f18d",
+    storageBucket: "fir-6f18d.appspot.com",
+    messagingSenderId: "1081976887309",
+    appId: "1:1081976887309:web:b47a10e7a1dd05be655d44",
+    measurementId: "G-6JQBHFKLJ2"
+  };
